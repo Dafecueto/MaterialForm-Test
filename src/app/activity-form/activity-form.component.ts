24f3bb7f7c293c1470dc5514c9596acbb3bf24ca
Dafecueto <dav0star1991@gmail.com>
2021-01-21 14:01:02 +0100

    userform

--- src/app/activity-form/activity-form.component.ts
+++ src/app/activity-form/activity-form.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-activity-form',
+  selector: 'app-details',
   templateUrl: './activity-form.component.html',
   styleUrls: ['./activity-form.component.scss']
 })
-export class ActivityFormComponent implements OnInit {
-  actform : FormGroup;
+export class DetailsComponent implements OnInit {
 
-  constructor(
-    private formBuilder : FormBuilder
-  ) { 
-    this.actform = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]]
-    })
+  userDetailsForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userDetailsForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(10)]],
+      age: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
+      date: ['', [Validators.required]]
+    });  
+  }
+
+  ngOnInit() {
+  }
+
+  submit = () => {
+
+    // service call to store the data
+  }
+
+  setFormControls = () => {
 
   }
 
+  invalidateForm = () => {
 
-  ngOnInit(): void {
   }
 
-  submit(event: Event) {
-    event.preventDefault();
-    if (!this.actform.valid) {
-      return;
-    }
-    console.log(this.actform.value);
-  }
+  // get username() { return this.userDetailsForm.get('username'); }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
-import { MatFormFieldModule } from '@angular/material/form-field'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ActivityFormComponent } from './activity-form/activity-form.component';
-import { MatDatepickerModule } from '@angular/material/datepicker'; 
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select'; 
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { DetailsComponent } from './activity-form/activity-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActivityFormComponent
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -27,8 +28,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatDatepickerModule,
+    FlexLayoutModule,
     MatNativeDateModule,
-    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
